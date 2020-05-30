0aed112487c23b9dda9415dcd4f2df23821b8354
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-05-30 17:02:10 +0500

    add  change code

--- test_items.py
+++ test_items.py
@@ -2,8 +2,8 @@ import time
 
 
 def test_find_button(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/")
-    time.sleep(10)
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
     button = browser.find_element_by_class_name('btn-add-to-basket')
     assert button.is_displayed(), 'No find!'
     time.sleep(5)
