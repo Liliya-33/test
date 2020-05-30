44d855b608b82306ce0112a009a9d05d7da4b352
Liliya-33 <liliya.kadyrova.0106@gmail.com>
2020-05-30 17:01:49 +0500

    add  change code

--- conftest.py
+++ conftest.py
@@ -1,26 +1,11 @@
-"""У меня не удалось проверить тест в браузере Chrome, так как выхожит ошибка 'ERROR test_items.py::test_find_button
-- selenium.common.exceptions.SessionNotCreatedException: Message: session not created: This version of ChromeDriver only
- supports Chrome version 81', то есть не совпадают версии, ошибку исправить не удалось. В браузере Firefox все проходит
- отлично! Пожалуйста, не снижайте за это баллы! Спасибо!!!"""
-
-
 from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="firefox", help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='es', help="Choose lang")
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None, help="Choose lang")
 
 
 @pytest.fixture(scope="function")
@@ -28,6 +13,7 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
